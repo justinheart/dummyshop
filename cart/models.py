8ec8009dc7202ccdec2d7e22862bf554632cc775
Justin Chang <justin2018777@gmail.com>
2020-03-22 02:08:10 +0800

    [feature]01:place an order [what] 1. Create Product and Order models, including related unit tests. 2. Create a seed file for Product model. [why] Prepare the data model for placing an order feature.

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_id = models.IntegerField(primary_key=True)
+    stock_pcs = models.IntegerField(default=None, blank=True, null=True)
+    price = models.IntegerField(default=None, blank=True, null=True)
+    shop_id = models.CharField(max_length=2, default=None, blank=True, null=True)
+    vip = models.BooleanField(default=None, blank=True, null=True)
+
+class Order(models.Model):
+    order_id = models.IntegerField(primary_key=True)
+    product = models.ForeignKey('Product', to_field='product_id', on_delete=models.CASCADE)
+    qty = models.IntegerField(default=None, blank=True, null=True)
+    price = models.IntegerField(default=None, blank=True, null=True)
+    shop_id = models.CharField(default=None, blank=True, null=True, max_length=2)
+    customer_id = models.IntegerField(default=None, blank=True, null=True)
--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,95 @@
 from django.test import TestCase
+from cart.models import Product, Order
 
 # Create your tests here.
+class ProductModelTest(TestCase):
+
+    def test_saving_and_retrieving_products(self):
+        first_product = Product()
+        first_product.product_id = 1
+        first_product.stock_pcs = 6
+        first_product.price = 150
+        first_product.shop_id = 'um'
+        first_product.vip = False
+        first_product.save()
+
+        second_product = Product()
+        second_product.product_id = 2
+        second_product.stock_pcs = 10
+        second_product.price = 800
+        second_product.shop_id = 'ps'
+        second_product.vip = True
+        second_product.save()
+
+        saved_products = Product.objects.all()
+        self.assertEqual(saved_products.count(), 2)
+
+        first_saved_product = saved_products[0]
+        self.assertEqual(first_saved_product.product_id, 1)
+        self.assertEqual(first_saved_product.stock_pcs, 6)
+        self.assertEqual(first_saved_product.price, 150)
+        self.assertEqual(first_saved_product.shop_id, 'um')
+        self.assertEqual(first_saved_product.vip, False)
+
+        second_saved_product = saved_products[1]
+        self.assertEqual(second_saved_product.product_id, 2)
+        self.assertEqual(second_saved_product.stock_pcs, 10)
+        self.assertEqual(second_saved_product.price, 800)
+        self.assertEqual(second_saved_product.shop_id, 'ps')
+        self.assertEqual(second_saved_product.vip, True)
+
+    def test_saving_and_retrieving_orders(self):
+        first_product = Product()
+        first_product.product_id = 1
+        first_product.stock_pcs = 6
+        first_product.price = 150
+        first_product.shop_id = 'um'
+        first_product.vip = False
+        first_product.save()
+
+        second_product = Product()
+        second_product.product_id = 2
+        second_product.stock_pcs = 10
+        second_product.price = 800
+        second_product.shop_id = 'ps'
+        second_product.vip = True
+        second_product.save()
+
+        saved_products = Product.objects.all()
+
+        first_order = Order(product=saved_products[0])
+        first_order.order_id = 1
+        first_order.product_id = 1
+        first_order.qty = 6
+        first_order.price = saved_products[0].price
+        first_order.shop_id = saved_products[0].shop_id
+        first_order.customer_id = 1
+        first_order.save()
+
+        second_order = Order(product=saved_products[1])
+        second_order.order_id = 2
+        second_order.product_id = saved_products[1].product_id
+        second_order.qty = 3
+        second_order.price = saved_products[1].price
+        second_order.shop_id = saved_products[1].shop_id
+        second_order.customer_id = 2
+        second_order.save()
+
+        saved_orders = Order.objects.all()
+        self.assertEqual(saved_orders.count(), 2)
+
+        first_saved_order = saved_orders[0]
+        self.assertEqual(first_saved_order.order_id, 1)
+        self.assertEqual(first_saved_order.product_id, 1)
+        self.assertEqual(first_saved_order.qty, 6)
+        self.assertEqual(first_saved_order.price, 150)
+        self.assertEqual(first_saved_order.shop_id, 'um')
+        self.assertEqual(first_saved_order.customer_id, 1)
+
+        second_saved_order = saved_orders[1]
+        self.assertEqual(second_saved_order.order_id, 2)
+        self.assertEqual(second_saved_order.product_id, 2)
+        self.assertEqual(second_saved_order.qty, 3)
+        self.assertEqual(second_saved_order.price,800)
+        self.assertEqual(second_saved_order.shop_id, 'ps')
+        self.assertEqual(second_saved_order.customer_id, 2)
